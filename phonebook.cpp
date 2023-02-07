19cfec748f49076e46a24dee78adb3f213f2d938
Royalty <akannie33@gmail.com>
2023-02-07 14:41:09 +0100

    phonebook example

---
+++ phonebook.cpp
@@ -0,0 +1,12 @@
+#include "hash_table.hpp"
+
+int main()
+{
+    struct HashMap *phonebook = initHashMap(1024);
+    insert(phonebook, "mama", "09065345");
+    insert(phonebook, "dada", "01566324");
+    printMap(phonebook);
+    // OMG!!!! I have to change mama's phone no. she gat a new one! 😲
+    insert(phonebook, "mama", "08065354");
+    printMap(phonebook);
+}
